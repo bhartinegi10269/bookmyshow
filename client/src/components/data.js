570714350288ec70b2acda520c6998fc09058ede
bhartinegi <bhartinegi10269@gmail.com>
2024-08-17 10:02:59 +0530

    first commit

---
+++ client/src/components/data.js
@@ -0,0 +1,6 @@
+let movies = ['Stree2', 'Yeh jawani hai Deewani', 'Interstellar', 'Laila Majnu', 'La La Land','Hangover','Aviator']
+let slots = ['10:00 AM', '01:00 PM', '03:00 PM', "07:00 PM", "10:00 PM"]
+let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2']
+exports.movies = movies;
+exports.slots = slots;
+exports.seats = seats;
---
+++ client/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, "public"),
+        filename: "index_bundle.js",
+        publicPath: '/', // Add this line to specify the public URL of the output directory
+    },
+    devServer: {
+        proxy: {
+            '/api/**': {
+                target: 'http://localhost:8080/',
+                secure: false,
+                changeOrigin: true
+            }
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$|\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                ]
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    }
+};
